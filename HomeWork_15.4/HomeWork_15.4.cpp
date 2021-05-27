05907d9d0627109069eaad70aa67e25a9fcf9df2
Kirill Vorozhishchev <1507kirill1998@mail.ru>
2021-05-27 22:03:29 +0700

    Добавьте файлы проекта.

---
+++ HomeWork_15.4/HomeWork_15.4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	const int N = 15;
+	int i = 0;
+
+	for (i = 0; i < N; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			std::cout << "Even__" << i << '\n';
+		}
+
+		else
+		{
+			std::cout << "Odd__" << i << '\n';
+		}
+	}
+}
